8370a4df3e593da62e6124a594a8bf07ed3dd7f7
fentosv <fentos.dev@gmail.com>
2024-04-23 17:58:17 +0200

    Move states to props and remove children as prop

--- src/components/ConfirmToast/index.tsx
+++ src/components/ConfirmToast/index.tsx
@@ -1,31 +1,33 @@
-import React, { useState } from 'react'
+import React from 'react'
 import clsx from 'clsx'
 import { CloseIcon } from '../CloseIcon'
 import styles from './styles.module.css'
 
 interface Props {
   asModal?: boolean
-  children: React.ReactNode
   childrenClassName?: string
-  showCloseIcon?: boolean
   customCancel?: string
   customConfirm?: string
   customFunction: () => void
   message?: string
   position?: 'bottom-left' | 'bottom-right' | 'top-left' | 'top-right'
+  setShowConfirmToast: React.Dispatch<React.SetStateAction<boolean>>
+  showCloseIcon?: boolean
+  showConfirmToast: boolean
   theme?: 'light' | 'dark' | 'snow' | 'lilac'
 }
 
 export function ConfirmToast({
   asModal = false,
-  children,
   childrenClassName,
-  showCloseIcon = true,
   customCancel = 'Cancel',
   customConfirm = 'Ok',
   customFunction,
   message = 'Do you want to continue?',
   position = 'bottom-right',
+  setShowConfirmToast,
+  showCloseIcon = true,
+  showConfirmToast,
   theme = 'light',
 }: Props) {
   function Wrapper({
@@ -49,8 +51,6 @@ export function ConfirmToast({
     return <>{children}</>
   }
 
-  const [showConfirmToast, setShowConfirmToast] = useState(false)
-
   const closeModal = (e: React.MouseEvent): void => {
     e.stopPropagation()
     setShowConfirmToast(false)
@@ -63,59 +63,58 @@ export function ConfirmToast({
     }
   }
 
-  const classes = clsx(styles.container, {
-    [styles[position]]: asModal ? false : position, //if asModal, don't return 'position' class
-    [styles['modal-content']]: asModal,
-  })
+  const classes = clsx(
+    styles.container,
+    {
+      [styles[position]]: asModal ? false : position, //if asModal, don't return 'position' class
+      [styles['modal-content']]: asModal,
+    },
+    styles[theme],
+    childrenClassName
+  )
 
   return (
-    <div
-      onClick={() => setShowConfirmToast(true)}
-      className={clsx(styles[theme], childrenClassName)}
-    >
-      {showConfirmToast && (
-        <Wrapper asModal={asModal}>
-          <output className={classes}>
-            <div className={styles['title-wrapper']}>
-              <p className={styles.title}>{message}</p>
-              {showCloseIcon && (
-                <button
-                  className={styles['close-button']}
-                  onClick={(e) => {
-                    closeModal(e)
-                  }}
-                >
-                  <CloseIcon
-                    aria-label='close modal'
-                    className={styles['close-svg']}
-                  />
-                </button>
-              )}
-            </div>
+    showConfirmToast && (
+      <Wrapper asModal={asModal}>
+        <output className={classes}>
+          <div className={styles['title-wrapper']}>
+            <p className={styles.title}>{message}</p>
+            {showCloseIcon && (
+              <button
+                className={styles['close-button']}
+                onClick={(e) => {
+                  closeModal(e)
+                }}
+              >
+                <CloseIcon
+                  aria-label='close modal'
+                  className={styles['close-svg']}
+                />
+              </button>
+            )}
+          </div>
 
-            <div className={styles['buttons-container']}>
-              <button
-                className={`${styles.button} ${styles['button--yes']}`}
-                onClick={(e) => {
-                  customFunction()
-                  closeModal(e)
-                }}
-              >
-                {customConfirm}
-              </button>
-              <button
-                className={`${styles.button} ${styles['button--no']}`}
-                onClick={(e) => {
-                  closeModal(e)
-                }}
-              >
-                {customCancel}
-              </button>
-            </div>
-          </output>
-        </Wrapper>
-      )}
-      {children}
-    </div>
+          <div className={styles['buttons-container']}>
+            <button
+              className={`${styles.button} ${styles['button--yes']}`}
+              onClick={(e) => {
+                customFunction()
+                closeModal(e)
+              }}
+            >
+              {customConfirm}
+            </button>
+            <button
+              className={`${styles.button} ${styles['button--no']}`}
+              onClick={(e) => {
+                closeModal(e)
+              }}
+            >
+              {customCancel}
+            </button>
+          </div>
+        </output>
+      </Wrapper>
+    )
   )
 }
