cb98b6a483424c299f39f4eeffda859d55d54097
fentosv <fentos.dev@gmail.com>
2024-04-24 17:58:09 +0200

    Make ConfirmToast stories works with state and setState

--- src/components/ConfirmToast/ConfirmToast.stories.tsx
+++ src/components/ConfirmToast/ConfirmToast.stories.tsx
@@ -1,70 +1,53 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryFn } from '@storybook/react'
 import { fn } from '@storybook/test'
 
-import { ConfirmToast } from '.'
+import { ConfirmToast, ConfirmToastProps } from '.'
 import { useState } from 'react'
-
-function ConfirmToastStories(args: Partial<unknown>) {
-  const [open, setOpen] = useState(false)
-
-  return (
-    <>
-      <button onClick={() => setOpen(!open)}>External Click me!</button>
-      <ConfirmToast
-        showConfirmToast={open}
-        setShowConfirmToast={setOpen}
-        // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-        customFunction={fn}
-        {...args}
-      />
-    </>
-  )
-}
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Components/ConfirmToast',
-  component: ConfirmToastStories,
+  component: ConfirmToast,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-} satisfies Meta<typeof ConfirmToastStories>
+  args: {
+    customFunction: fn(),
+  },
+} satisfies Meta<typeof ConfirmToast>
 
 export default meta
-type Story = StoryObj<typeof meta>
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-
-export const AsModal: Story = {
-  args: {
-    theme: 'light',
-    asModal: true,
-  },
+const Template: StoryFn<ConfirmToastProps> = (args) => {
+  const [open, setOpen] = useState(true)
+  return (
+    <>
+      <button onClick={() => setOpen(!open)}>External Click me!</button>
+      <ConfirmToast
+        {...args}
+        showConfirmToast={open}
+        setShowConfirmToast={setOpen}
+      />
+    </>
+  )
 }
 
-export const Light: Story = {
-  args: {
-    theme: 'light',
-  },
-}
+export const Default = Template.bind({})
 
-export const Dark: Story = {
-  args: {
-    theme: 'dark',
-  },
-}
+export const AsModal = Template.bind({})
+AsModal.args = { asModal: true }
 
-export const Lilac: Story = {
-  args: {
-    theme: 'lilac',
-  },
-}
+export const Light = Template.bind({})
+Light.args = { theme: 'light' }
 
-export const Snow: Story = {
-  args: {
-    theme: 'snow',
-  },
-}
+export const Dark = Template.bind({})
+Dark.args = { theme: 'dark' }
+
+export const Lilac = Template.bind({})
+Lilac.args = { theme: 'lilac' }
+
+export const Snow = Template.bind({})
+Snow.args = { theme: 'snow' }
