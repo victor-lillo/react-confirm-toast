f0b8bf42c30f03fdca705f009e035a0692b1dad6
fentosv <fentos.dev@gmail.com>
2024-05-03 20:52:39 +0200

    Change test environment to happy-dom

--- vite.config.ts
+++ vite.config.ts
@@ -51,7 +51,7 @@ export default defineConfig({
   },
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: 'happy-dom',
     setupFiles: './src/test/setup.ts',
     // you might want to disable it, if you don't have tests that rely on CSS
     // since parsing CSS is slow
